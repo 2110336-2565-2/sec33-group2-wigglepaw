62f0e381ca5a5218c9b55ac6ef44006b9a7a9dde
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-01-25 18:52:44 +0700

    Decorated the demo

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -18,17 +18,23 @@ const DemoPage: NextPage = () => {
       <h3>Traverse into the depth of valley</h3>
     </section>
 
-    <section>
-      <h2 className='font-bold underline decoration-2'>Here is the fibonacci of {n}</h2>
-      {fibo.isLoading && <p>Loading...</p>}
-      {fibo.isError && <p>Error: {fibo.error.message}</p>}
-      <p>The fibonacci of <input type="number" value={n} onChange={ele => setN(ele.target.valueAsNumber)}></input> is {fibo.data}</p>
-    </section> 
+    <hr className="border-t-2 border-gray-200"/>
+
+  <section>
+    <h2 className='font-bold underline decoration-2'>Here is the fibonacci of {n}</h2>
+    {fibo.isLoading && <p>Loading...</p>}
+    {fibo.isError && <p>Error: {fibo.error.message}</p>}
+    <p>The fibonacci of <input type="number" value={n} onChange={ele => setN(ele.target.valueAsNumber)}></input> is {fibo.data}</p>
+  </section> 
   
+    <hr className="border-t-2 border-gray-200"/>
+
     <section>
       <h2 className='font-bold underline decoration-2'>Here is all the pet</h2>
       <PetList />
     </section>
+
+    <hr className="border-t-2 border-gray-200"/>
 
     <section>
     <h2 className='font-bold underline decoration-2'>{"Let's add a pet"}</h2>
@@ -39,33 +45,39 @@ const DemoPage: NextPage = () => {
 }
 
 const PetList: React.FC = () => {
-  const pets = api.pet.getAllPet.useQuery();
+  const pets = api.pet.getAllPet.useQuery()
 
-  return <div>
-    {pets.isLoading && <p>Loading...</p>}
-    {pets.isError && <p>Error: {pets.error.message}</p>}
-    <table>
+  if (pets.isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (pets.isError) {
+    return <p className='text-red-400'>Error: {pets.error.message}</p>
+  }
+
+  return <div className="flex flex-col items-center">
+    <h1 className="text-3xl font-bold mb-8">Pets</h1>
+    <table className="table-auto border-collapse border border-gray-400">
       <thead>
-        <tr>
-          <th>Name</th>
-          <th>Kind</th>
-          <th>Owner Id</th>
-          <th>Owner Name</th>
-          <th>Owner Email</th>
+        <tr className="bg-gray-400">
+          <th className="border border-gray-400 px-4">Name</th>
+          <th className="border border-gray-400 px-4">Kind</th>
+          <th className="border border-gray-400 px-4">Owner Id</th>
+          <th className="border border-gray-400 px-4">Owner Name</th>
+          <th className="border border-gray-400 px-4">Owner Email</th>
         </tr>
       </thead>
       <tbody>
-        {pets.data?.map(pet => <tr key={pet.id}>
-          <td className="px-4">{pet.name}</td>
-          <td className="px-4">{pet.kind}</td>
-          <td className="px-4">{pet.owner.id}</td>
-          <td className="px-4">{pet.owner.name}</td>
-          <td className="px-4">{pet.owner.email}</td>
+        {pets.data?.map(pet => <tr key={pet.id} className="bg-gray-200">
+          <td className="border border-gray-400 px-4">{pet.name}</td>
+          <td className="border border-gray-400 px-4">{pet.kind}</td>
+          <td className="border border-gray-400 px-4">{pet.owner.id}</td>
+          <td className="border border-gray-400 px-4">{pet.owner.name}</td>
+          <td className="border border-gray-400 px-4">{pet.owner.email}</td>
         </tr>)}
       </tbody>
     </table>
   </div>
-  
 }
 
 const PetShow: React.FC = () => {
@@ -95,31 +107,27 @@ const PetShow: React.FC = () => {
   }
 
   // Form laydown the input vertically using tailwind
-  return <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-    {/*Pet name text input*/}
-    <label htmlFor="name">Pet name</label>
-    <input className="border-blue-400" type="text" id="name" {...register("name", {required: true})} />
-    <p className='text-red-400'>{errors.name?.message}</p>
+  return <div className="flex flex-col gap-2">
+  <label htmlFor="name">Pet name</label>
+  <input className="border-blue-400 bg-blue-100" type="text" id="name" {...register("name", {required: true})} />
+  <p className='text-red-400'>{errors.name?.message}</p>
 
-    {/*Pet type select: doggo or neko*/}
-    <label htmlFor="kind">Pet type</label>
-    <select id="kind" {...register("kind", {required: true})}>
-      <option value="DOGGO">Doggo</option>
-      <option value="NEKO">Neko</option>
-    </select>
-    <p className='text-red-400'>{errors.kind?.message}</p>
+  <label htmlFor="kind">Pet type</label>
+  <select id="kind" {...register("kind", {required: true })}>
+    <option value="DOGGO">Doggo</option>
+    <option value="NEKO">Neko</option>
+  </select>
+  <p className='text-red-400'>{errors.kind?.message}</p>
 
-    {/*Owner id select, display owner's name to user, ow*/}
-    <label htmlFor="ownerId">Owner id</label>
-    <select id="ownerId" {...register("ownerId", {required: true})}>
-      {owners.data?.map(owner => <option key={owner.id} value={owner.id}>{owner.name}</option>)}
-    </select>
-    <p className='text-red-400'>{errors.ownerId?.message}</p>
+  <label htmlFor="ownerId">Owner id</label>
+  <select id="ownerId" {...register("ownerId", {required: true})}>
+    {owners.data?.map(owner => <option key={owner.id} value={owner.id}>{owner.name}</option>)}
+  </select>
+  <p className='text-red-400'>{errors.ownerId?.message}</p>
 
-    {/*Sumit button*/}
-    <input type="submit" />
 
-  </form>;
+  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit">Add pet</button>
+</div>;
 }
 
 export default DemoPage
