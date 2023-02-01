7b47585b31b4bb9df52231593aca3d33d72926e4
KawinM23 <kawin.m235@gmail.com>
2023-02-01 20:18:54 +0700

    Create matching.tsx

---
+++ src/pages/matching.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Header from "../components/Header";
+
+const matching: NextPage = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default matching;
