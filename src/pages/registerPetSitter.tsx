9407749163de17c293638ef9558b9f7e2d34b66c
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-02-03 01:01:13 +0900

    finish first ver, w8 Jo to check

--- src/pages/registerPetSitter.tsx
+++ src/pages/registerPetSitter.tsx
@@ -16,33 +16,113 @@ export default function RegisterPetSitter() {
     businessLicense: "", //If PetHotel
     hotelName: "",
   });
-  const [type, setType] = useState("");
-  var test = 1;
+  const [page1, setPage1] = useState({
+    username: "",
+    password: "",
+    phoneNumber: "",
+    email: "",
+    address: "",
+    type: "",
+  });
+  const [page2hotel, setPage2hotel] = useState({
+    hotelName: "",
+    businessLicense: "",
+  });
+  const [page2free, setPage2free] = useState({
+    firstname: "",
+    lastname: "",
+  });
+  const [page3, setPage3] = useState({
+    bankAccount: "",
+    bankName: "",
+    cvv: "",
+    exp: "",
+    cardno: "",
+  });
+
   const [state, setState] = useState(0); //0=main info 1=freelance 2=hotelpet
 
-  const sendProfileinfo = async (e: {
+  const onSubmitpage1 = (e: {
     target: any;
-    preventDefault: () => void;
+    preventDefault(): unknown;
+    e: { preventDefault: any };
+    cat: string;
+  }) => {
+    e.preventDefault();
+    setPage1({
+      username: e.target.username.value,
+      password: e.target.password.value,
+      phoneNumber: e.target.phone.value,
+      email: e.target.email.value,
+      address: e.target.ads.value,
+      type: e.target.choice.value,
+    });
+
+    //console.log(page1);
+    if (e.target.choice.value === "petfreelance") {
+      setState(1);
+    } else {
+      setState(2);
+    }
+  };
+
+  const onSubmitpage2 = (e: {
+    target: any;
+    preventDefault(): unknown;
+    e: { preventDefault: any };
+    cat: string;
+  }) => {
+    e.preventDefault();
+    if (page1.type === "petfreelance") {
+      setPage2free({
+        firstname: e.target.firstname.value,
+        lastname: e.target.lastname.value,
+      });
+    } else {
+      setPage2hotel({
+        hotelName: e.target.hotelname.value,
+        businessLicense: e.target.blicense.value,
+      });
+    }
+    //console.log(page2free, page2hotel);
+    setState(3);
+  };
+  const onSubmitpage3 = (e: {
+    target: any;
+    preventDefault(): unknown;
+    e: { preventDefault: any };
+    cat: string;
   }) => {
     e.preventDefault();
 
-    const studentinfo = {
-      firstname: e.target.fname.value,
-      lastname: e.target.lname.value,
-      age: e.target.age.value,
-      tel: e.target.phone.value,
-      psub: e.target.psub.value,
-      des: e.target.descrip.value,
-      link: e.target.link.value,
+    var data = {
+      bankAccount: e.target.bankno.value,
+      bankName: e.target.bankname.value,
+      cvv: e.target.cvv.value,
+      exp: e.target.exp.value,
+      cardno: e.target.cardno.value,
     };
+
+    var real = {};
+    if (page1.type === "petfreelance") {
+      real = Object.assign({}, page1, page2free, data);
+    } else {
+      real = Object.assign({}, page1, page2hotel, data);
+    }
+    alert(JSON.stringify(real)); //real data ready to be sent to backends
+
+    setState(3);
   };
+
+  const printData = () => {};
 
   const FirstPage = () => {
     if (state === 1) {
       return (
         <>
           <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
-          <form className="w-4/5">
+          <h1 className="py-2 text-3xl">2/3</h1>
+          <form className="w-4/5" onSubmit={onSubmitpage2}>
             <div className="grid  grid-cols-1 grid-rows-6">
               <div className="m-3">
                 <label className="mb-2 block text-sm font-medium text-gray-900 ">
@@ -50,7 +130,9 @@ export default function RegisterPetSitter() {
                 </label>
                 <input
                   type="text"
-                  id="username"
+                  id="firstname"
+                  defaultValue={page2free.firstname}
+                  placeholder="ทอมมี่"
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
@@ -60,7 +142,9 @@ export default function RegisterPetSitter() {
                 </label>
                 <input
                   type="text"
-                  id="username"
+                  id="lastname"
+                  defaultValue={page2free.lastname}
+                  placeholder="มานาบุญ"
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
@@ -75,10 +159,7 @@ export default function RegisterPetSitter() {
                   <p>Back</p>
                 </button>
                 <button
-                  type="button"
-                  onClick={(e: { target: any }) => {
-                    setState(3);
-                  }}
+                  type="submit"
                   className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
                 >
                   <p>Next</p>
@@ -92,7 +173,8 @@ export default function RegisterPetSitter() {
       return (
         <>
           <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
-          <form className="w-4/5">
+          <h1 className="py-2 text-3xl">2/3</h1>
+          <form className="w-4/5" onSubmit={onSubmitpage2}>
             <div className="grid  grid-cols-1 grid-rows-6">
               <div className="m-3">
                 <label className="mb-2 block text-sm font-medium text-gray-900 ">
@@ -100,17 +182,21 @@ export default function RegisterPetSitter() {
                 </label>
                 <input
                   type="text"
-                  id="username"
+                  id="hotelname"
+                  defaultValue={page2hotel.hotelName}
+                  placeholder="Muse Hotel At Anatara Resort and Spa and Sport club and Therapy and University "
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
               <div className="m-3">
                 <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Lastname
+                  Business License
                 </label>
                 <input
                   type="text"
-                  id="username"
+                  id="blicense"
+                  defaultValue={page2hotel.businessLicense}
+                  placeholder="WUT DA FUCK IS THIS SLOT DO :<"
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
@@ -125,10 +211,7 @@ export default function RegisterPetSitter() {
                   <p>Back</p>
                 </button>
                 <button
-                  type="button"
-                  onClick={(e: { target: any }) => {
-                    setState(3);
-                  }}
+                  type="submit"
                   className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
                 >
                   <p>Next</p>
@@ -138,11 +221,111 @@ export default function RegisterPetSitter() {
           </form>
         </>
       );
+    } else if (state === 3) {
+      return (
+        <>
+          <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
+          <h1 className="py-2 text-3xl">3/3</h1>
+          <div className="flex w-2/3 justify-center">
+            <form className=" h-full w-full pt-5 " onSubmit={onSubmitpage3}>
+              <div className="mx-auto grid w-full grid-cols-2 grid-rows-6 gap-5 md:grid-cols-2 md:gap-2">
+                <div className="col-span-4 flex items-center">
+                  <input className="mr-2" type="checkbox"></input>
+                  <label>By Card</label>
+                  <div className="ml-4 h-6 w-8 rounded  bg-blue-300"></div>
+                  <div className="ml-2 h-6 w-8 rounded  bg-blue-300"></div>
+                  <div className="ml-2 h-6 w-8 rounded  bg-blue-300"></div>
+                </div>
+                <div className="col-span-2 flex w-full  flex-col">
+                  <label>Card No*</label>
+                  <input
+                    id="cardno"
+                    placeholder="xxxx xxxx xxxx xxxx"
+                    type="number"
+                    className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                  />
+                </div>
+                <div className="col-span-2"></div>
+
+                <div className="flex w-full flex-col">
+                  <label>Expiration Date*</label>
+                  <input
+                    id="exp"
+                    className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                    type="date"
+                  />
+                </div>
+                <div className="flex w-full flex-col">
+                  <label>CVV / CVN*</label>
+                  <input
+                    id="cvv"
+                    className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                    type="number"
+                  />
+                </div>
+                <div className="col-span-2"></div>
+                <div className="col-span-4 flex w-full items-center">
+                  <input className="mr-2" type="checkbox"></input>
+                  <label>Mobile banking</label>
+                  <div className="ml-4 h-7 w-7 rounded-full bg-blue-300"></div>
+                  <div className="ml-2 h-7 w-7 rounded-full bg-blue-300"></div>
+                  <div className="ml-2 h-7 w-7 rounded-full bg-blue-300"></div>
+                  <div className="ml-2 h-7 w-7 rounded-full bg-blue-300"></div>
+                </div>
+                <div className="flex w-full flex-col">
+                  <label>Bank No*</label>
+                  <input
+                    id="bankno"
+                    className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                    placeholder="xxx-x-xxxxx-x"
+                    type="number"
+                  />
+                </div>
+                <div className=" flex w-full flex-col">
+                  <label>Bank Name*</label>
+                  <input
+                    id="bankname"
+                    placeholder="ABC"
+                    className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                  />
+                </div>
+                <div className="col-span-2"></div>
+                <div className="col-span-2 flex items-center">
+                  <input className="mr-2" type="checkbox"></input>
+                  <div>
+                    <label>I agree to the &nbsp;</label>
+                    <label className="text-red-600 underline">
+                      terms, conditions and privacy policy
+                    </label>
+                  </div>
+                </div>
+
+                <div className="flex gap-6"></div>
+              </div>
+              <div className="mt-5 flex w-full justify-evenly">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setState(0);
+                  }}
+                  className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  Back
+                </button>
+                <button className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16">
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
+      );
     } else {
       return (
         <>
           <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
-          <form className="w-4/5">
+          <h1 className="py-2 text-3xl">1/3</h1>
+          <form className="w-4/5" onSubmit={onSubmitpage1}>
             <div className="grid  grid-cols-1 grid-rows-6">
               <div className="m-3">
                 <label className="mb-2 block text-sm font-medium text-gray-900 ">
@@ -151,6 +334,8 @@ export default function RegisterPetSitter() {
                 <input
                   type="text"
                   id="username"
+                  placeholder="Kimmy"
+                  defaultValue={page1.username}
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
@@ -161,6 +346,8 @@ export default function RegisterPetSitter() {
                 <input
                   type="email"
                   id="email"
+                  placeholder="Jommy@gmail.com"
+                  defaultValue={page1.email}
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
@@ -181,6 +368,8 @@ export default function RegisterPetSitter() {
                 <input
                   type="tel"
                   id="phone"
+                  placeholder="0123456789"
+                  defaultValue={page1.phoneNumber}
                   name="phone"
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
@@ -191,12 +380,14 @@ export default function RegisterPetSitter() {
                 </label>
                 <input
                   type="text"
-                  id="psub"
-                  name="psub"
+                  id="ads"
+                  name="ads"
+                  placeholder="Wat Gu Yang Soi 2 :)"
+                  defaultValue={page1.address}
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
-              {/* <div className=" mx-3 flex justify-between py-2 pb-6">
+              <div className=" mx-3 flex justify-between py-2 pb-6">
                 <div className="flex flex-wrap content-center items-center">
                   <label className="mb-2 block text-sm font-medium text-gray-900 ">
                     Choose your role here
@@ -204,36 +395,32 @@ export default function RegisterPetSitter() {
                   <select
                     id="choice"
                     name="choice"
+                    defaultValue={page1.type}
                     className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                    onChange={(e) => {
-                      setType(e.target.value);
-                    }}
                   >
                     <option value="petfreelance">Freelance Pet Sitter</option>
                     <option value="pethotel">Pet Hotel</option>
                   </select>
                 </div>
-              </div> */}
+              </div>
 
               <div className="mx-3 flex flex-wrap content-center items-center justify-evenly">
                 <button
-                  type="button"
-                  onClick={() => {
-                    setState(1);
-                  }}
+                  type="submit"
+                  // onClick={() => {
+                  //   console.log("??");
+                  //   setState(1);
+                  // }}
                   className=" rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
                 >
-                  <p>Next As Hotel Pet</p>
+                  <p>Next</p>
                 </button>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setState(2);
-                  }}
+                {/* <button
+                  type="submit"
                   className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
                 >
                   <p>Next As Freelance Pet</p>
-                </button>
+                </button> */}
               </div>
             </div>
           </form>
