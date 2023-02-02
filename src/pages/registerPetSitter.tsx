fcfc38479366f8ee78098d1198b6c27f59cea59e
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-02-02 16:22:44 +0900

    fix:bug page, done choice for hotel and freelance

--- src/pages/registerPetSitter.tsx
+++ src/pages/registerPetSitter.tsx
@@ -1,4 +1,3 @@
-import { type } from "os";
 import { useEffect, useState } from "react";
 
 export default function RegisterPetSitter() {
@@ -18,6 +17,7 @@ export default function RegisterPetSitter() {
     hotelName: "",
   });
   const [type, setType] = useState("");
+  var test = 1;
   const [state, setState] = useState(0); //0=main info 1=freelance 2=hotelpet
 
   const sendProfileinfo = async (e: {
@@ -64,18 +64,76 @@ export default function RegisterPetSitter() {
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
+              <div className="mx-3 flex flex-wrap content-end items-center justify-evenly">
+                <button
+                  type="button"
+                  onClick={(e: { target: any }) => {
+                    setState(0);
+                  }}
+                  className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  <p>Back</p>
+                </button>
+                <button
+                  type="button"
+                  onClick={(e: { target: any }) => {
+                    setState(3);
+                  }}
+                  className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  <p>Next</p>
+                </button>
+              </div>
             </div>
-
-            <div className="mx-3 flex flex-wrap content-end items-center justify-center">
-              <button
-                type="button"
-                onClick={(e: { target: any }) => {
-                  setState(0);
-                }}
-                className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
-              >
-                <p>Next</p>
-              </button>
+          </form>
+        </>
+      );
+    } else if (state === 2) {
+      return (
+        <>
+          <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
+          <form className="w-4/5">
+            <div className="grid  grid-cols-1 grid-rows-6">
+              <div className="m-3">
+                <label className="mb-2 block text-sm font-medium text-gray-900 ">
+                  Hotel
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                />
+              </div>
+              <div className="m-3">
+                <label className="mb-2 block text-sm font-medium text-gray-900 ">
+                  Lastname
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+                />
+              </div>
+              <div className="mx-3 flex flex-wrap content-end items-center justify-evenly">
+                <button
+                  type="button"
+                  onClick={(e: { target: any }) => {
+                    setState(0);
+                  }}
+                  className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  <p>Back</p>
+                </button>
+                <button
+                  type="button"
+                  onClick={(e: { target: any }) => {
+                    setState(3);
+                  }}
+                  className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  <p>Next</p>
+                </button>
+              </div>
             </div>
           </form>
         </>
@@ -138,7 +196,7 @@ export default function RegisterPetSitter() {
                   className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
               </div>
-              <div className=" mx-3 flex justify-between py-2 pb-6">
+              {/* <div className=" mx-3 flex justify-between py-2 pb-6">
                 <div className="flex flex-wrap content-center items-center">
                   <label className="mb-2 block text-sm font-medium text-gray-900 ">
                     Choose your role here
@@ -147,35 +205,34 @@ export default function RegisterPetSitter() {
                     id="choice"
                     name="choice"
                     className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                    required
                     onChange={(e) => {
                       setType(e.target.value);
                     }}
                   >
-                    <option selected disabled>
-                      <p>Choose here</p>
-                    </option>
-                    <option value="petfreelance">
-                      <p>Freelance Pet Sitter</p>
-                    </option>
-                    <option value="pethotel">
-                      <p>Pet Hotel</p>
-                    </option>
+                    <option value="petfreelance">Freelance Pet Sitter</option>
+                    <option value="pethotel">Pet Hotel</option>
                   </select>
                 </div>
-              </div>
+              </div> */}
 
-              <div className="mx-3 flex flex-wrap content-end items-center justify-center">
+              <div className="mx-3 flex flex-wrap content-center items-center justify-evenly">
                 <button
                   type="button"
-                  onClick={(e: { target: any }) => {
-                    if (type === "petfreelance") {
-                      setState(1);
-                    }
+                  onClick={() => {
+                    setState(1);
+                  }}
+                  className=" rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
+                >
+                  <p>Next As Hotel Pet</p>
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setState(2);
                   }}
                   className="rounded bg-[#98AAB4] px-6  py-2.5 text-center text-sm font-semibold text-[#213951] drop-shadow-md hover:bg-[#8b9ba3] focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto md:px-16"
                 >
-                  <p>Next</p>
+                  <p>Next As Freelance Pet</p>
                 </button>
               </div>
             </div>
