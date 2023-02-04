390cac386fef5658356f68ca517936b95357ac3d
KawinM23 <kawin.m235@gmail.com>
2023-02-04 15:56:29 +0700

    Create PetSitterCard

---
+++ src/components/PetSitterCard.tsx
@@ -0,0 +1,5 @@
+const PetSitterCard = () => {
+  return <div className=""></div>;
+};
+
+export default PetSitterCard;
--- src/pages/matching.tsx
+++ src/pages/matching.tsx
@@ -2,11 +2,32 @@ import { NextPage } from "next";
 import Header from "../components/Header";
 import SearchBox from "../components/SearchBox";
 
-const matching: NextPage = () => {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getStaticProps() {
+  const allusers = await prisma.user.findMany();
+
+  return {
+    props: {
+      users: allusers,
+    },
+  };
+}
+
+const matching: NextPage = (props: any) => {
+  const users = props.users;
+
   return (
     <div>
       <Header></Header>
       <SearchBox></SearchBox>
+      <div>
+        {users.map((user: any) => (
+          <div className="card">{user.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
