a754d45d5a8fd1f67570f9cd58c38d5cb1913d61
KawinM23 <kawin.m235@gmail.com>
2023-02-04 18:31:52 +0700

    Add data and link to PetSitterCard

--- src/components/PetSitterCard.tsx
+++ src/components/PetSitterCard.tsx
@@ -1,5 +1,27 @@
-const PetSitterCard = () => {
-  return <div className=""></div>;
+import Image from "next/image";
+import Link from "next/link";
+
+const PetSitterCard = (pet_sitter: any) => {
+  return (
+    <Link
+      href="/user/12345" // TODO: Link to correct user
+      className="m-2 flex h-fit rounded-2xl bg-amber-200 p-2 transition-colors hover:bg-amber-100"
+    >
+      <div className="relative m-2 flex w-[5rem]">
+        <Image
+          src={"/profile_icon.png"}
+          alt={"Profile Pic"}
+          fill
+          className="object-contain"
+        ></Image>
+      </div>
+      <div className="ml-2 w-[40%] leading-8">
+        <div>Pet Sitter: Tuchathum Sunkameka</div>
+        <div>Pet Type</div>
+        <div>Rating</div>
+      </div>
+    </Link>
+  );
 };
 
 export default PetSitterCard;
