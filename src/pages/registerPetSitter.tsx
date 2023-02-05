ddcf3c3340408c7226716bd326fb91943634bc37
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-02-05 22:38:26 +0900

    fix: now use ZOD with react Form, fix page1 and styling

--- src/pages/registerPetSitter.tsx
+++ src/pages/registerPetSitter.tsx
@@ -1,4 +1,16 @@
-import { useEffect, useState } from "react";
+import * as React from "react";
+import { useState } from "react";
+import type { NextPage } from "next";
+import { api } from "../utils/api";
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormRegister,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { PetKind } from "@prisma/client";
 
 export default function RegisterPetSitter() {
   const [editmode, setEditmode] = useState(false);
@@ -321,80 +333,127 @@ export default function RegisterPetSitter() {
         </>
       );
     } else {
+      const validationSchema = z.object({
+        // like a schema for register in form
+        username: z.string().min(2, { message: "come on! longer than that!" }),
+        email: z
+          .string()
+          .min(2, { message: "longer" })
+          .max(12, { message: "shorter" }),
+        password: z.string().min(1, { message: "Don't leave it blank" }),
+        phonenumber: z.string(),
+        // .length(10, { message: "phoenumber must have a fix length of 10" }),   For the sake of the tester, for real case don't forget to remove comment
+        address: z.string().min(1, { message: "required" }),
+        type: z.string(),
+      });
+      type ValidationSchema = z.infer<typeof validationSchema>;
+
+      const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<ValidationSchema>({
+        resolver: zodResolver(validationSchema), //using zod object above and link it with react hook
+      });
+
+      const onSubmit: SubmitHandler<ValidationSchema> = (data, e?: Event) => {
+        e?.preventDefault();
+        setPage1({
+          username: data.username,
+          password: data.password,
+          phoneNumber: data.phonenumber,
+          email: data.email,
+          address: data.address,
+          type: data.type,
+        });
+        if (data.type === "petfreelance") {
+          setState(1);
+        } else {
+          setState(2);
+        }
+
+        alert(JSON.stringify(data));
+      };
+
       return (
         <>
           <h1 className="py-2 text-3xl">Register Pet Sitter</h1>
           <h1 className="py-2 text-3xl">1/3</h1>
-          <form className="w-4/5" onSubmit={onSubmitpage1}>
+          <form className="w-4/5" onSubmit={handleSubmit(onSubmit)}>
             <div className="grid  grid-cols-1 grid-rows-6">
-              <div className="m-3">
-                <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Username
-                </label>
-                <input
-                  type="text"
+              <div className="mx-2">
+                <Input
                   id="username"
-                  placeholder="Kimmy"
+                  label="Username"
                   defaultValue={page1.username}
-                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                />
-              </div>
-              <div className="mx-3">
-                <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Jommy@gmail.com"
-                  defaultValue={page1.email}
-                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                />
-              </div>
-              <div className="mx-3">
-                <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                />
-              </div>
-              <div className="mx-3">
-                <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Telephone Number
-                </label>
-                <input
-                  type="tel"
-                  id="phone"
-                  placeholder="0123456789"
-                  defaultValue={page1.phoneNumber}
-                  name="phone"
-                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
-                />
-              </div>
-              <div className="mx-3">
-                <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                  Address
-                </label>
-                <input
+                  register={register}
+                  validationRules={{ required: true }}
+                  placeholder="goddamn"
                   type="text"
-                  id="ads"
-                  name="ads"
-                  placeholder="Wat Gu Yang Soi 2 :)"
-                  defaultValue={page1.address}
-                  className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                 />
+                {errors.username && (
+                  <p className="errorstyle"> {errors.username?.message}</p>
+                )}
               </div>
-              <div className=" mx-3 flex justify-between py-2 pb-6">
+              <div className="mx-2">
+                <Input
+                  id="email"
+                  label="Email"
+                  defaultValue={page1.email}
+                  register={register}
+                  validationRules={{ required: true }}
+                  placeholder="gg@ggwp"
+                  type="email"
+                />
+                {errors.email && (
+                  <p className="errorstyle"> {errors.email?.message}</p>
+                )}
+              </div>
+              <div className="mx-2">
+                <Input
+                  id="password"
+                  label="Password"
+                  register={register}
+                  validationRules={{ required: true }}
+                  type="password"
+                />
+                {errors.password && (
+                  <p className="errorstyle"> {errors.password?.message}</p>
+                )}
+              </div>
+              <div className="mx-2">
+                <Input
+                  id="phonenumber"
+                  label="Phone Number"
+                  defaultValue={page1.phoneNumber}
+                  register={register}
+                  validationRules={{ required: true }}
+                  type="number"
+                />
+                {errors.phonenumber && (
+                  <p className="errorstyle"> {errors.phonenumber?.message}</p>
+                )}
+              </div>
+              <div className="mx-2">
+                <Input
+                  id="address"
+                  label="Address"
+                  register={register}
+                  defaultValue={page1.address}
+                  validationRules={{ required: true }}
+                  type="text"
+                />
+                {errors.address && (
+                  <p className="errorstyle"> {errors.address?.message}</p>
+                )}
+              </div>
+              <div className=" mx-2 flex justify-between py-2 pb-6">
                 <div className="flex flex-wrap content-center items-center">
                   <label className="mb-2 block text-sm font-medium text-gray-900 ">
                     Choose your role here
                   </label>
                   <select
-                    id="choice"
-                    name="choice"
+                    {...register("type")}
                     defaultValue={page1.type}
                     className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
                   >
@@ -437,3 +496,32 @@ export default function RegisterPetSitter() {
     </>
   );
 }
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  label: string;
+  register: UseFormRegister<FieldValues>; // declare register props
+  validationRules?: object;
+}
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  register,
+  validationRules,
+  type = "text",
+  defaultValue,
+  ...rest
+}) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+
+    <input
+      className="block w-full rounded border border-gray-100 bg-gray-100 p-1 px-2 text-sm text-gray-900 drop-shadow-md focus:border-blue-500 focus:bg-white focus:ring-blue-500"
+      id={id}
+      type={type}
+      defaultValue={defaultValue}
+      {...rest}
+      {...register(id, validationRules)}
+    />
+  </>
+);
