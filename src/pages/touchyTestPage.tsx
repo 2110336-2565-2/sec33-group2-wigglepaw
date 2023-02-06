a91e0b1e79dbcbbc2a3b3f12f57efe759dafb3b5
Tuchtham Sungkameka <touch0869@gmail.com>
2023-02-06 21:48:45 +0700

    CRUD for users done

--- src/pages/touchyTestPage.tsx
+++ src/pages/touchyTestPage.tsx
@@ -6,62 +6,69 @@ import React, { useState } from "react";
 
 import Header from "../components/Header";
 const touchyTestPage = () => {
-    const greet = api.touchy.greetings.useQuery({name:"Touchy69"});
-    const user = api.user.getByUsername.useQuery({username:"touchy"});
-    const user2 = api.user.getByUserId.useQuery({userId:"cldpsw73e0000u5usuvisdw23"});
-    const user3 = api.user.getByEmail.useQuery({email:"kawin69@gmail.com"});
-    const [x,setX] = useState(0);
-    const postUserAPI = api.user.post.useMutation();
-    const deleteUserAPI = api.user.deleteByUsername.useMutation();
-    if (!greet.data) {
-        return (
-            <div style={styleCenter}>
-                <h1>Loading...</h1>
-            </div>
-        );
-    }
+  const greet = api.touchy.greetings.useQuery({ name: "Touchy69" });
+  const user = api.user.getByUsername.useQuery({ username: "touchy" });
+  const user2 = api.user.getByUserId.useQuery({
+    userId: "cldpsw73e0000u5usuvisdw23",
+  });
+  const user3 = api.user.getByEmail.useQuery({ email: "kawin69@gmail.com" });
+  const [x, setX] = useState(0);
+  const postUserAPI = api.user.post.useMutation();
+  const deleteUserAPI = api.user.deleteByUsername.useMutation();
+  if (!greet.data) {
+    return (
+      <div style={styleCenter}>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
-    const buttonHandlerPostUser = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        postUserAPI.mutate([{username:"touchy",email:"touch69@gmail.com"},
-                            {username:"Son",email:"kawin69@gmail.com"}
-                            ]);
-      };
-      const buttonHandlerDeleteUser = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        deleteUserAPI.mutate({username:["touchy","Son"]});
-      };
+  const buttonHandlerPostUser = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    postUserAPI.mutate([
+      { username: "touchy", email: "touch69@gmail.com" },
+      { username: "Son", email: "kawin69@gmail.com" },
+    ]);
+  };
+  const buttonHandlerDeleteUser = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    deleteUserAPI.mutate({ username: ["touchy", "Son"] });
+  };
 
   return (
     <div style={styleCenter}>
-        <h1>
+      <h1>
         get username<br></br>
-        {JSON.stringify(user.data)}<br></br>
+        {JSON.stringify(user.data)}
+        <br></br>
         get id <br></br>
-        {JSON.stringify(user2.data)}<br></br>
+        {JSON.stringify(user2.data)}
+        <br></br>
         get email <br></br>
-        {JSON.stringify(user3.data)}<br></br>
+        {JSON.stringify(user3.data)}
+        <br></br>
         {x}
-        </h1>
+      </h1>
 
-        <div>
+      <div>
         <button onClick={buttonHandlerPostUser}>Post</button> <br></br>
-        <button onClick={buttonHandlerDeleteUser}>Delete</button>  
-        </div>
-        
-
+        <button onClick={buttonHandlerDeleteUser}>Delete</button>
+      </div>
     </div>
-);
-}
+  );
+};
 
 const styleCenter = {
-    width: '100vw',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-
-}
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 export default touchyTestPage;
--- src/server/api/routers/userRouter.ts
+++ src/server/api/routers/userRouter.ts
@@ -1,26 +1,27 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC } from "@trpc/server";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "../../../env/server.mjs";
 import { createTRPCContext } from "../../../server/api/trpc";
 import { appRouter } from "../../../server/api/root";
-import { z } from 'zod';
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 const zodUserFields = z.object({
-    userId: z.string().cuid().optional(),
-    username: z.string(),
-    email: z.string().email(),
-    emailVerified: z.date().optional(),
-    phoneNumber: z.string().optional(),
-    address: z.string().optional(),
-    imageUri: z.string().optional(),
-    backAccount: z.string().optional(),
-    backName: z.string().optional(),
-    // accounts: Account
+  //userId: z.string().cuid().optional(),
+  username: z.string(),
+  email: z.string().email(),
+  phoneNumber: z.string().optional(),
+  address: z.string().optional(),
+  imageUri: z.string().optional(),
+  backAccount: z.string().optional(),
+  backName: z.string().optional(),
+  // accounts: Account
 });
 
 export const userRouter = createTRPCRouter({
-    getByUserId: publicProcedure.input(z.object({userId:z.string()})).query(({ ctx, input }) => {
+  getByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(({ ctx, input }) => {
       return ctx.prisma.user.findMany({
         where: {
           userId: input.userId,
@@ -28,46 +29,68 @@ export const userRouter = createTRPCRouter({
       });
     }),
 
-    getByUsername: publicProcedure.input(z.object({username:z.string()})).query(({ ctx, input }) => {
-        return ctx.prisma.user.findMany({
-          where: {
-            username: input.username,
-          },
-        });
-      }),
-
-    getByEmail: publicProcedure.input(z.object({email:z.string()})).query(({ ctx, input }) => {
-    return ctx.prisma.user.findMany({
+  getByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.findMany({
         where: {
-            email: input.email,
+          username: input.username,
         },
-    });
+      });
     }),
 
-    // post: publicProcedure.input(z.object(userFields)).query(({ ctx, input }) => {
-    post: publicProcedure.input(z.array(zodUserFields)).mutation(({ ctx, input }) => {
-        return ctx.prisma.user.createMany({
-            data: input
-          });
-        }),
+  getByEmail: publicProcedure
+    .input(z.object({ email: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.findMany({
+        where: {
+          email: input.email,
+        },
+      });
+    }),
 
-    deleteById: publicProcedure.input(z.object({userId:z.array(z.string())})).mutation(({ ctx, input }) => {
+  // post: publicProcedure.input(z.object(userFields)).query(({ ctx, input }) => {
+  post: publicProcedure
+    .input(z.array(zodUserFields))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.user.createMany({
+        data: input,
+      });
+    }),
+
+  deleteById: publicProcedure
+    .input(z.object({ userIds: z.array(z.string()) }))
+    .mutation(({ ctx, input }) => {
       return ctx.prisma.user.deleteMany({
-          where: {
-            userId: {
-              in: input.userId
-            }
-          }
-        });
-      }),
+        where: {
+          userId: {
+            in: input.userIds,
+          },
+        },
+      });
+    }),
 
-      deleteByUsername: publicProcedure.input(z.object({username:z.array(z.string())})).mutation(({ ctx, input }) => {
-        return ctx.prisma.user.deleteMany({
-            where: {
-              username: {
-                in: input.username
-              }
-            }
-          });
-        }),
-})
+  deleteByUsername: publicProcedure
+    .input(z.object({ usernames: z.array(z.string()) }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.user.deleteMany({
+        where: {
+          username: {
+            in: input.usernames,
+          },
+        },
+      });
+    }),
+
+  deleteByEmail: publicProcedure
+    .input(z.object({ emails: z.array(z.string()) }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.user.deleteMany({
+        where: {
+          email: {
+            in: input.emails,
+          },
+        },
+      });
+    }),
+});
