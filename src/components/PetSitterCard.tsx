b338c9eb1a134b7c1d839c78d1d6a14202114e22
KawinM23 <kawin.m235@gmail.com>
2023-02-06 20:00:07 +0700

    Format PetSitterCard more reponsive

--- src/components/PetSitterCard.tsx
+++ src/components/PetSitterCard.tsx
@@ -9,9 +9,9 @@ const PetSitterCard = (props: any) => {
   return (
     <Link
       href={profile_link} // TODO: Link to correct user
-      className="m-2 flex h-fit rounded-2xl bg-amber-200 p-2 transition-colors hover:bg-amber-100"
+      className="m-2 flex h-fit rounded-2xl bg-amber-200 p-3 transition-colors hover:bg-amber-100 md:p-5"
     >
-      <div className="relative m-2 flex w-[5rem]">
+      <div className="relative flex w-[4rem] md:w-[5rem]">
         <Image
           src={"/profile_icon.png"}
           alt={"Profile Pic"}
@@ -19,10 +19,14 @@ const PetSitterCard = (props: any) => {
           className="object-contain"
         ></Image>
       </div>
-      <div className="ml-2 w-[40%] leading-8">
-        <div>Pet Sitter: {name} </div>
-        <div>Pet Type</div>
-        <div>Rating</div>
+      <div className="mx-3 w-fit leading-6">
+        <p>Pet Sitter: {name} </p>
+        <p>Pet Type</p>
+        <p>Rating</p>
+      </div>
+      <div className="my-auto ml-auto w-fit text-center md:mr-4">
+        <p>Distance:</p>
+        <p>5.0 km</p>
       </div>
     </Link>
   );
