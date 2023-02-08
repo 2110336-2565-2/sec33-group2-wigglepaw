ccf155121c8335d39d822b92dc4e0a0477e0cf25
Tuchtham Sungkameka <touch0869@gmail.com>
2023-02-08 11:16:43 +0700

    Implemented deleteById for user with on-cascade delete

--- src/server/api/routers/userRouter.ts
+++ src/server/api/routers/userRouter.ts
@@ -12,75 +12,18 @@ import {
 } from "../../../schema/schema";
 
 export const userRouter = createTRPCRouter({
-  getByUserId: publicProcedure
-    .input(z.object({ userId: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.user.findMany({
-        where: {
-          userId: input.userId,
-        },
-      });
-    }),
-
-  getByUsername: publicProcedure
-    .input(z.object({ username: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.user.findMany({
-        where: {
-          username: input.username,
-        },
-      });
-    }),
-
-  getByEmail: publicProcedure
-    .input(z.object({ email: z.string() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.user.findMany({
-        where: {
-          email: input.email,
-        },
-      });
-    }),
-
-  // post: publicProcedure.input(z.object(userFields)).query(({ ctx, input }) => {
   post: publicProcedure.input(userFields).mutation(({ ctx, input }) => {
     return ctx.prisma.user.create({
       data: input,
     });
   }),
 
-  deleteById: publicProcedure
-    .input(z.object({ userIds: z.array(z.string()) }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.user.deleteMany({
+  deleteByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.user.delete({
         where: {
-          userId: {
-            in: input.userIds,
-          },
-        },
-      });
-    }),
-
-  deleteByUsername: publicProcedure
-    .input(z.object({ usernames: z.array(z.string()) }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.user.deleteMany({
-        where: {
-          username: {
-            in: input.usernames,
-          },
-        },
-      });
-    }),
-
-  deleteByEmail: publicProcedure
-    .input(z.object({ emails: z.array(z.string()) }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.user.deleteMany({
-        where: {
-          email: {
-            in: input.emails,
-          },
+          userId: input.userId,
         },
       });
     }),
