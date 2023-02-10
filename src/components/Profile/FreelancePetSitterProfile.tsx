0a1eeef696c1983964b790173a7bb8a6fc41522f
KawinM23 <kawin.m235@gmail.com>
2023-02-10 22:38:42 +0700

    Add form in edit mode

--- src/components/Profile/FreelancePetSitterProfile.tsx
+++ src/components/Profile/FreelancePetSitterProfile.tsx
@@ -11,9 +11,20 @@ import {
 } from "react-icons/hi";
 import { IoPaw } from "react-icons/io5";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const FreelancePetSitterProfile = (props: any) => {
   const [editing, setEditing] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  console.log(watch("example"));
 
   return (
     <div>
@@ -28,7 +39,7 @@ const FreelancePetSitterProfile = (props: any) => {
           </h1>
           {props.editable && (
             <button
-              onClick={() => setEditing(true)}
+              onClick={() => setEditing(!editing)}
               className="mx-auto flex whitespace-nowrap rounded-2xl bg-sky-700 px-3 py-1 text-center font-semibold text-white hover:bg-sky-600"
             >
               Edit my profile{" "}
@@ -57,6 +68,48 @@ const FreelancePetSitterProfile = (props: any) => {
             <p className="data-field">
               <IoPaw className="profile-icon" /> Pet Types:{" "}
             </p>
+          </div>
+        )}
+        {editing && (
+          <div className="justify-auto mt-2 flex w-screen flex-wrap rounded-md border-[3px] border-sky-500 p-2 md:mt-0 md:w-3/5">
+            {/* <div >
+              <p className="data-field">
+                <HiUserCircle className="profile-icon" /> Name: Jhon Swogvagen
+              </p>
+              <p className="data-field">
+                <HiPhone className="profile-icon" /> Phone: 0123456789
+              </p>
+              <p className="data-field">
+                <HiMap className="profile-icon" />
+                &nbsp;Address: 254 Phaya Thai Rd, Wang Mai, Khet Pathum Wan,
+                Krung Thep Maha Nakhon 10330
+              </p>
+              <p className="data-field">
+                <HiAtSymbol className="profile-icon" />
+                &nbsp;Email: wiggle-paw@chula.ac.th
+              </p>
+              <p className="data-field">
+                <IoPaw className="profile-icon" /> Pet Types:{" "}
+              </p>
+            </div> */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* register your input into the hook by invoking the "register" function */}
+
+              <p className="data-field flex">
+                <HiUserCircle className="profile-icon" /> Name:&nbsp;
+                <input
+                  defaultValue="Jhon Swogvagen"
+                  placeholder="Name"
+                  className="inline w-full"
+                  {...register("firstName", { required: true })}
+                />
+              </p>
+
+              {/* errors will return when field validation fails  */}
+              {errors.exampleRequired && <span>This field is required</span>}
+
+              <input type="submit" />
+            </form>
           </div>
         )}
       </div>
