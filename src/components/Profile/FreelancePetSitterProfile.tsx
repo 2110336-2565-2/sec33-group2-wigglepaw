ed1630c45d10322e5337697c328ac51de05bee40
KawinM23 <kawin.m235@gmail.com>
2023-02-10 20:40:14 +0700

    Format profile and Add icons

--- src/components/Profile/FreelancePetSitterProfile.tsx
+++ src/components/Profile/FreelancePetSitterProfile.tsx
@@ -2,44 +2,59 @@ import Header from "../Header";
 import Image from "next/image";
 import Link from "next/link";
 
-import { HiPencilAlt } from "react-icons/hi";
-
-const infomationSection = "flex flex-wrap";
+import {
+  HiAtSymbol,
+  HiMap,
+  HiPencilAlt,
+  HiPhone,
+  HiUserCircle,
+} from "react-icons/hi";
+import { IoPaw } from "react-icons/io5";
 
 const FreelancePetSitterProfile = (props: any) => {
   return (
     <div>
       <Header></Header>
-      <div className={infomationSection}>
-        <div className="flex w-screen flex-col md:w-1/5">
+      <div className="mx-3 flex flex-wrap">
+        <div className="my-auto flex w-screen flex-col md:w-1/5 md:min-w-min">
           <div className="relative mx-auto flex h-[6rem] w-[6rem]">
             <Image src={"/profile_icon.png"} alt={"Icon"} fill></Image>
           </div>
-          <p className="mx-auto my-1 text-center text-xl font-semibold">
+          <h1 className="mx-auto my-1 text-center text-2xl font-semibold">
             {props.username}
-          </p>
+          </h1>
           <Link
             href={`/user/${props.username}/edit`}
-            className="mx-auto flex rounded-2xl bg-sky-700 px-3 py-1 text-center text-white hover:bg-sky-600"
+            className="mx-auto flex whitespace-nowrap rounded-2xl bg-sky-700 px-3 py-1 text-center font-semibold text-white hover:bg-sky-600"
           >
             Edit my profile{" "}
             <HiPencilAlt className="mt-auto mb-[0.2rem] ml-1 fill-white" />
           </Link>
         </div>
 
-        <div className="justify-auto mt-2 flex w-screen flex-wrap md:w-3/5">
-          <p className="w-full">Name: Jhon Swogvagen</p>
-          <p>Pet types: </p>
-          <p className="w-full">Phone: 0123456789</p>
-          <p className="w-full">
-            Address: 254 Phaya Thai Rd, Wang Mai, Khet Pathum Wan, Krung Thep
-            Maha Nakhon 10330
+        <div className="justify-auto mx-2 mt-2 flex w-screen flex-wrap md:mt-0 md:w-3/5">
+          <p className="data-field">
+            <HiUserCircle className="profile-icon" /> Name: Jhon Swogvagen
           </p>
-          <p className="w-full">Email: wiggle-paw@chula.ac.th</p>
+          <p className="data-field">
+            <HiPhone className="profile-icon" /> Phone: 0123456789
+          </p>
+          <p className="data-field">
+            <HiMap className="profile-icon" />
+            &nbsp;Address: 254 Phaya Thai Rd, Wang Mai, Khet Pathum Wan, Krung
+            Thep Maha Nakhon 10330
+          </p>
+          <p className="data-field">
+            <HiAtSymbol className="profile-icon" />
+            &nbsp;Email: wiggle-paw@chula.ac.th
+          </p>
+          <p className="data-field">
+            <IoPaw className="profile-icon" /> Pet Types:{" "}
+          </p>
         </div>
       </div>
-      <div className="mx-auto mt-3 w-screen max-w-md sm:w-1/2">
-        <h1 className="font-bold">Posts</h1>
+      <div className="mx-3 mt-2 max-w-md sm:w-1/2">
+        <h1 className="text-xl font-bold">Posts</h1>
         {/* TODO: Posts display */}
         {/* {users.map((user: any) => (
           <PetSitterCard pet_sitter={user}></PetSitterCard>
