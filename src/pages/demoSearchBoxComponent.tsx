155cb70768bc05aba57d601828eba4bcdedf464d
noWarm <jomwit22@gmail.com>
2023-02-20 03:53:01 +0700

    added searchbox header

--- src/pages/demoSearchBoxComponent.tsx
+++ src/pages/demoSearchBoxComponent.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { Fragment, useState } from "react";
 const petTypesList = [
   "Dog",
@@ -33,72 +34,84 @@ const DemoSearchBoxComponent: NextPage = () => {
   };
 
   return (
-    <main className="">
-      <div className="mx-auto h-screen w-[312px] border">
-        <div
-          id="search-box"
-          className="flex flex-col  rounded-xl bg-[#ffdfa0] px-7 pb-6"
-        >
-          <div id="header-search" className=" ">
-            <p className="text-[25px] font-bold">Search Pet Sitters</p>
+    <main className="pt-20">
+      <div className="mx-auto h-screen w-[351px] border">
+        <div id="search-box" className="relative drop-shadow-md">
+          <div id="header-search" className="">
+            <Image
+              alt="could be a shelter roof, could be a bone. who knows"
+              src="/searchBoxHeaderBg.png"
+              width={351}
+              height={91}
+              className="absolute"
+            />
+            <p className="absolute left-1 top-5 z-20 w-full text-center text-[25px] font-bold text-white drop-shadow-lg">
+              Search Pet Sitters
+            </p>
           </div>
-          <div id="search-params-wrapper" className="flex flex-col ">
-            <div
-              id="name-input-wrapper"
-              className="mb-2 flex flex-col border-b-[1px] border-[#c89d48] pb-4"
-            >
-              <p className="text-[15px] font-bold text-[#8a5534]">Name</p>
-              <input
-                className=" rounded-md border border-[#8a5534] px-2 py-1 text-[#b77b59] placeholder-[#c2c2c2] "
-                placeholder="Sitter Name"
-              ></input>
-            </div>
-            <div
-              id="pet-types-input-wrapper"
-              className="mb-2 flex flex-col border-b-[1px] border-[#c89d48] pb-4 "
-            >
-              <p className="mb-1 mt-1 text-[15px] font-bold text-[#8a5534]">
-                Pet Types
-              </p>
+          <div id="empty-space" className="mx-auto h-[15px] w-[312px] "></div>
+          <div
+            id="search-pane"
+            className="mx-auto flex w-[312px] flex-col rounded-xl bg-[#ffdfa0] px-7 pb-6 pt-16"
+          >
+            <div id="search-params-wrapper" className="flex flex-col ">
               <div
-                id="pet-types-selector"
-                className="grid grid-cols-2 gap-y-3 px-2"
+                id="name-input-wrapper"
+                className="mb-2 flex flex-col border-b-[1px] border-[#c89d48] pb-4"
               >
-                {petTypesList.map((petType) => (
-                  <Fragment key={petType}>
-                    <div className="flex flex-row gap-2">
-                      <div
-                        className="flex h-[25px] w-[25px] select-none flex-col justify-center rounded-sm text-center text-xl font-medium text-white"
-                        style={{ backgroundColor: getBgColor(petType) }}
-                        onClick={() => {
-                          toggleState(petType);
-                        }}
-                      >
-                        <input type="checkbox" hidden></input>
-                        {checkBoxState[petType] ? "✓" : ""}
+                <p className="text-[15px] font-bold text-[#8a5534]">Name</p>
+                <input
+                  className=" rounded-md border border-[#8a5534] px-2 py-1 text-[#b77b59] placeholder-[#c2c2c2] "
+                  placeholder="Sitter Name"
+                ></input>
+              </div>
+              <div
+                id="pet-types-input-wrapper"
+                className="mb-2 flex flex-col border-b-[1px] border-[#c89d48] pb-4 "
+              >
+                <p className="mb-1 mt-1 text-[15px] font-bold text-[#8a5534]">
+                  Pet Types
+                </p>
+                <div
+                  id="pet-types-selector"
+                  className="grid grid-cols-2 gap-y-3 px-2"
+                >
+                  {petTypesList.map((petType) => (
+                    <Fragment key={petType}>
+                      <div className="flex flex-row gap-2">
+                        <div
+                          className="flex h-[25px] w-[25px] select-none flex-col justify-center rounded-sm text-center text-xl font-medium text-white"
+                          style={{ backgroundColor: getBgColor(petType) }}
+                          onClick={() => {
+                            toggleState(petType);
+                          }}
+                        >
+                          <input type="checkbox" hidden></input>
+                          {checkBoxState[petType] ? "✓" : ""}
+                        </div>
+                        <p className="text-[15px] font-normal text-[#8a5534]">
+                          {petType}
+                        </p>
                       </div>
-                      <p className="text-[15px] font-normal text-[#8a5534]">
-                        {petType}
-                      </p>
-                    </div>
-                  </Fragment>
-                ))}
+                    </Fragment>
+                  ))}
+                </div>
+              </div>
+              <div id="price-range-input-wrapper" className="flex flex-col ">
+                <p className="text-[15px] font-bold text-[#8a5534]">
+                  Price Range
+                </p>
+                <div id="two-thumbs" className=" ">
+                  twoThumbs component & min max
+                </div>
               </div>
             </div>
-            <div id="price-range-input-wrapper" className="flex flex-col ">
-              <p className="text-[15px] font-bold text-[#8a5534]">
-                Price Range
-              </p>
-              <div id="two-thumbs" className=" ">
-                twoThumbs component & min max
-              </div>
-            </div>
-          </div>
 
-          <div id="search-button-wrapper" className="">
-            <button className="drop-shadow-slg w-full bg-[#2a4764] py-2 text-[25px] font-bold text-white hover:bg-[#213951]">
-              Search
-            </button>
+            <div id="search-button-wrapper" className="">
+              <button className="drop-shadow-slg w-full rounded-sm bg-[#2a4764] py-2 text-[25px] font-bold text-white hover:bg-[#213951]">
+                Search
+              </button>
+            </div>
           </div>
         </div>
       </div>
