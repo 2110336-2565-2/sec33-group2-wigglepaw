2319d73bac35aec4c72492cbc9cb63a8aa6a0dd6
Tuchtham Sungkameka <touch0869@gmail.com>
2023-02-19 15:48:55 +0700

    Debugged petTypes for owner, updated seeding with multiple petTypes and include more profile pictures at seeding

--- src/server/api/routers/seedRouter.ts
+++ src/server/api/routers/seedRouter.ts
@@ -107,7 +107,8 @@ async function makeOwner(
   lastName: string,
   phone: string,
   address: string,
-  imageUri: string
+  imageUri: string,
+  petTypes: string[]
 ) {
   return await prisma.petOwner.create({
     data: {
@@ -121,6 +122,8 @@ async function makeOwner(
           imageUri: imageUri,
         },
       },
+
+      petTypes: petTypes,
       firstName: firstName,
       lastName: lastName,
     },
@@ -130,21 +133,18 @@ async function makeOwner(
   });
 }
 
-// UTILITY
 function getMultipleRandom(arr: string[], num: number) {
   const shuffled = [...arr].sort(() => 0.5 - Math.random());
 
   return shuffled.slice(0, num);
 }
 
-function randomIntFromInterval(min: number, max: number) {
+function getRandomIntFromInterval(min: number, max: number) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-// UTILITY
 
-// INFO
-var firstNames: string[] = [
+const firstNames: string[] = [
   "Tokino",
   "Kanata",
   "Watame",
@@ -156,7 +156,7 @@ var firstNames: string[] = [
   "Matsuri",
   "Mei",
 ];
-var lastNames: string[] = [
+const lastNames: string[] = [
   "Amane",
   "Kiryuu",
   "Sora",
@@ -164,7 +164,7 @@ var lastNames: string[] = [
   "Rosenthal",
   "Sung086",
 ];
-var addresses: string[] = [
+const addresses: string[] = [
   "Home",
   "Bangkok somewhere",
   "USA",
@@ -173,7 +173,7 @@ var addresses: string[] = [
   "Isekai",
   "OtakuRoom",
 ];
-var petTypes: string[] = [
+const petTypes: string[] = [
   "Dog",
   "Cat",
   "Goldfish",
@@ -183,7 +183,19 @@ var petTypes: string[] = [
   "Lizard",
   "Hamster",
 ];
-// INFO
+
+const imageUris: string[] = [
+  "https://i.ppy.sh/547c489981845883d37a525d50d892d8c3d490e0/68747470733a2f2f692e696d6775722e636f6d2f5430586455414e2e6a7067",
+  "https://p4.wallpaperbetter.com/wallpaper/771/366/387/anime-anime-girls-hololive-kiryu-coco-redhead-hd-wallpaper-preview.jpg",
+  "https://img-9gag-fun.9cache.com/photo/aZrDB5Q_460s.jpg",
+  "https://preview.redd.it/8p4t0a95ax161.jpg?auto=webp&s=9c9db601dfdf217647380cbffe44d7eaf0efa47b",
+  "https://gamerbraves.sgp1.cdn.digitaloceanspaces.com/2022/02/Rushia_FI2.jpg",
+  "https://hololive.hololivepro.com/wp-content/uploads/2022/06/%E5%8D%9A%E8%A1%A3%E3%81%93%E3%82%88%E3%82%8A_WAO_jk-1.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGYXihhLktMheIVxxgkU3ouhnXf9F9wbxlhQ&usqp=CAU",
+  "https://cdn.donmai.us/original/98/83/98834cd37301a94a6a524d94f1ff5a88.jpg",
+  "https://static.wikia.nocookie.net/omniversal-battlefield/images/b/bd/Council.jpg/revision/latest?cb=20220420023638",
+  "https://picsum.photos/200",
+];
 
 export const seedRouter = createTRPCRouter({
   seed1: publicProcedure
@@ -195,34 +207,41 @@ export const seedRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       if (input.clearDatabase) {
-        await prisma.user.deleteMany({});
+        await ctx.prisma.user.deleteMany({});
       }
-      for (var i = 0; i < input.numberOfUsers; i++) {
-        var firstName = getMultipleRandom(firstNames, 1)[0] ?? "";
-        var lastName = getMultipleRandom(lastNames, 1)[0] ?? "";
-        var hotelName = firstName + " " + lastName + " Hotel";
-        var address = getMultipleRandom(addresses, 1)[0] ?? "";
-        var phoneNumber = "1234569780";
-        var petType = getMultipleRandom(petTypes, randomIntFromInterval(1, 3));
-        var startPrice = randomIntFromInterval(100, 300);
-        var endPrice = startPrice + randomIntFromInterval(100, 3000);
-        var imageUri = "https://picsum.photos/200";
-        switch (randomIntFromInterval(1, 3)) {
+
+      for (let i = 0; i < input.numberOfUsers; i++) {
+        const firstName = getMultipleRandom(firstNames, 1)[0] ?? "";
+        const lastName = getMultipleRandom(lastNames, 1)[0] ?? "";
+        const hotelName = firstName + " " + lastName + " Hotel";
+        const address = getMultipleRandom(addresses, 1)[0] ?? "";
+        const phoneNumber = "1234569780";
+        const petType = getMultipleRandom(
+          petTypes,
+          getRandomIntFromInterval(1, 3)
+        );
+        const startPrice = getRandomIntFromInterval(100, 300);
+        const endPrice = startPrice + getRandomIntFromInterval(100, 3000);
+        const imageUri = getMultipleRandom(imageUris, 1)[0] ?? "";
+
+        switch (getRandomIntFromInterval(1, 3)) {
           case 1: {
-            var code = "Owner" + randomIntFromInterval(100, 999);
-            makeOwner(
+            const code =
+              "Owner" + getRandomIntFromInterval(100, 999).toString();
+            await makeOwner(
               code,
               firstName,
               lastName,
               phoneNumber,
               address,
-              imageUri
+              imageUri,
+              petType
             );
             break;
           }
           case 2: {
-            var code = "Free" + randomIntFromInterval(100, 999);
-            makeFree(
+            const code = "Free" + getRandomIntFromInterval(100, 999).toString();
+            await makeFree(
               code,
               firstName,
               lastName,
@@ -236,8 +255,9 @@ export const seedRouter = createTRPCRouter({
             break;
           }
           default: {
-            var code = "Hotel" + randomIntFromInterval(100, 999);
-            makeHotel(
+            const code =
+              "Hotel" + getRandomIntFromInterval(100, 999).toString();
+            await makeHotel(
               code,
               hotelName,
               phoneNumber,
@@ -251,6 +271,5 @@ export const seedRouter = createTRPCRouter({
           }
         }
       }
-      return;
     }),
 });
