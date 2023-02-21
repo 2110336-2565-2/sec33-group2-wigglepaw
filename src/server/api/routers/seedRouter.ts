2399e7e872e0c550a8ea269374d6edbfb0ee87bc
Tuchtham Sungkameka <touch0869@gmail.com>
2023-02-21 14:51:29 +0700

    Seed for reviews

--- src/server/api/routers/seedRouter.ts
+++ src/server/api/routers/seedRouter.ts
@@ -13,6 +13,33 @@ import {
 import { Prisma } from "@prisma/client";
 import { prisma } from "../../db";
 
+async function updateAvgRating(petSitterId: string) {
+  const petSitter = await prisma.petSitter.findFirst({
+    where: {
+      userId: petSitterId,
+    },
+    include: {
+      review: true,
+    },
+  });
+  const reviews = petSitter?.review;
+  if (!reviews) return;
+
+  let sum = 0;
+  for (const review of reviews) {
+    sum += review.rating;
+  }
+  const avg = sum / reviews.length;
+
+  const update = await prisma.petSitter.update({
+    where: {
+      userId: petSitterId,
+    },
+    data: { avgRating: avg },
+  });
+  return update;
+}
+
 async function makeFree(
   code: string,
   firstname: string,
@@ -136,6 +163,71 @@ async function makeOwner(
   });
 }
 
+async function makeReview(
+  petSitterId: string,
+  petOwnerId: string,
+  rating: number,
+  text: string
+) {
+  const createReview = await prisma.review.create({
+    data: {
+      petSitterId: petSitterId,
+      petOwnerId: petOwnerId,
+      rating: rating,
+      text: text,
+      createdAt: new Date(),
+    },
+  });
+  const reviewId = createReview.reviewId;
+
+  await prisma.petOwner.update({
+    where: {
+      userId: petOwnerId,
+    },
+    data: {
+      review: {
+        connect: {
+          reviewId: reviewId,
+        },
+      },
+    },
+  });
+
+  await prisma.petSitter.update({
+    where: {
+      userId: petSitterId,
+    },
+    data: {
+      review: {
+        connect: {
+          reviewId: reviewId,
+        },
+      },
+    },
+  });
+
+  await updateAvgRating(petSitterId);
+  return createReview;
+}
+
+async function makePost(
+  petSitterId: string,
+  text: string,
+  pictureUri: string,
+  videoUri: string
+) {
+  const createPost = await prisma.post.create({
+    data: {
+      petSitterId: petSitterId,
+      text: text,
+      pictureUri: pictureUri,
+      videoUri: videoUri,
+      createdAt: new Date(),
+    },
+  });
+  return createPost;
+}
+
 function getMultipleRandom(arr: string[], num: number) {
   const shuffled = [...arr].sort(() => 0.5 - Math.random());
 
@@ -200,16 +292,24 @@ const imageUris: string[] = [
   "https://picsum.photos/200",
 ];
 
+const reviewTexts: string[] = [
+  "My dog said it was great.",
+  "good... I guess?",
+  "dog shit",
+  "Pretty nice",
+  "Skibidi pop pop pop pop ye ye ye ye",
+];
+
 export const seedRouter = createTRPCRouter({
-  seed1: publicProcedure
+  seedUsers: publicProcedure
     .input(
       z.object({
-        clearDatabase: z.boolean(),
+        clearUsers: z.boolean(),
         numberOfUsers: z.number(),
       })
     )
     .mutation(async ({ ctx, input }) => {
-      if (input.clearDatabase) {
+      if (input.clearUsers) {
         await ctx.prisma.user.deleteMany({});
       }
 
@@ -273,6 +373,43 @@ export const seedRouter = createTRPCRouter({
             break;
           }
         }
+      }
+    }),
+
+  seedReviews: publicProcedure
+    .input(
+      z.object({
+        clearReviews: z.boolean(),
+        numberOfReviews: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (input.clearReviews) {
+        await ctx.prisma.review.deleteMany({});
+      }
+
+      const sitters = await prisma.petSitter.findMany();
+      const owners = await prisma.petOwner.findMany();
+
+      const sitterIds = new Array<string>(input.numberOfReviews);
+      const ownerIds = new Array<string>(input.numberOfReviews);
+
+      for (
+        let i = 0;
+        i < Math.min(input.numberOfReviews, sitters.length);
+        i++
+      ) {
+        sitterIds[i] = sitters[i]?.userId ?? "";
+        ownerIds[i] = owners[i]?.userId ?? "";
+      }
+
+      for (let i = 0; i < input.numberOfReviews; i++) {
+        const sitterId = getMultipleRandom(sitterIds, 1)[0] ?? "";
+        const ownerId = getMultipleRandom(ownerIds, 1)[0] ?? "";
+        const rating = getRandomIntFromInterval(1, 5);
+        const text = getMultipleRandom(reviewTexts, 1)[0] ?? "";
+
+        await makeReview(sitterId, ownerId, rating, text);
       }
     }),
 });
