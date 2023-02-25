ed713a0cb64860c4e55bb2479d5049f5bc71f78e
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-02-25 20:24:45 +0700

    🐛 Post: fix delete button shown other's post

--- src/components/Profile/Post.tsx
+++ src/components/Profile/Post.tsx
@@ -12,6 +12,7 @@ import ReactTimeAgo from "react-time-ago";
 import { IoEllipsisVertical, IoTrash } from "react-icons/io5";
 import { Popover } from "@headlessui/react";
 import { api } from "../../utils/api";
+import { useSession } from "next-auth/react";
 
 TimeAgo.addLocale(en);
 TimeAgo.addDefaultLocale(en);
@@ -22,6 +23,7 @@ const Post = (props: { post: Post }) => {
 
   // API
   const utils = api.useContext();
+  const { data: session } = useSession();
   const deletePost = api.post.delete.useMutation({
     onSuccess: () => utils.petSitter.getPostsByUserId.invalidate(),
   });
@@ -77,25 +79,27 @@ const Post = (props: { post: Post }) => {
           <ReactTimeAgo date={props.post.createdAt} locale="en-US" />
 
           {/* Kebab menu (three dots) */}
-          <Popover className="relative inline">
-            <Popover.Button>
-              <IoEllipsisVertical className="ml-2 inline" />
-            </Popover.Button>
-            <Popover.Panel className="absolute z-10 w-32 rounded-md border-2 bg-white shadow-lg">
-              <div className="flex flex-col">
-                {/* Delete button */}
-                <button
-                  className="flex items-center justify-center rounded-md p-2 hover:bg-red-100"
-                  onClick={() =>
-                    deletePost.mutate({ postId: props.post.postId })
-                  }
-                >
-                  <IoTrash className="mr-2 inline" />
-                  <span className="text-red-500">Delete</span>
-                </button>
-              </div>
-            </Popover.Panel>
-          </Popover>
+          {session?.user?.userId == props.post.petSitterId && (
+            <Popover className="relative inline">
+              <Popover.Button>
+                <IoEllipsisVertical className="ml-2 inline" />
+              </Popover.Button>
+              <Popover.Panel className="absolute z-10 w-32 rounded-md border-2 bg-white shadow-lg">
+                <div className="flex flex-col">
+                  {/* Delete button */}
+                  <button
+                    className="flex items-center justify-center rounded-md p-2 hover:bg-red-100"
+                    onClick={() =>
+                      deletePost.mutate({ postId: props.post.postId })
+                    }
+                  >
+                    <IoTrash className="mr-2 inline" />
+                    <span className="text-red-500">Delete</span>
+                  </button>
+                </div>
+              </Popover.Panel>
+            </Popover>
+          )}
         </h2>
       </div>
       <p className="my-1 text-gray-700">{props.post.text}</p>
