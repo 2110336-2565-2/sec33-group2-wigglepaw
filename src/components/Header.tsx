eec9d6c649736b6ea8f54779dfe5c816b79fc6bd
KawinM23 <kawin.m235@gmail.com>
2023-02-27 19:34:02 +0700

    Use Menu in header
    Use headless UI Menu in header
    Add Custom color in tailwind config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,13 @@ import { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { api } from "../utils/api";
+import { Menu } from "@headlessui/react";
 
 const Header = () => {
   const { data: session, status } = useSession();
-  const isLoggedIn = status === "authenticated";
+  const isLoggedIn = true; //status === "authenticated"
   const logout = () => signOut();
   const username = session?.user?.username;
-
-  const [openRegister, setOpen] = useState(false);
-  const [openProfile, setOpenProfile] = useState(false);
-  const toggleRegister = () => setOpen(!openRegister);
-  const toggleProfile = () => setOpenProfile(!openProfile);
 
   const router = useRouter();
   const asPath = router.asPath;
@@ -32,215 +28,280 @@ const Header = () => {
     : "/profiledummy.png";
 
   return (
-    <span className="mb-4 flex h-fit w-screen bg-sky-900 md:pr-2">
-      <nav className="flex w-full">
+    <span className="mb-4 flex h-fit w-screen bg-wp-blue md:pr-2">
+      <nav className="flex w-full justify-between">
         <Link href={"/"} className="flex shrink-0">
           <div className="relative m-2 flex h-[4rem] w-[5rem]">
-            <Image src={"/logo_w.png"} alt={""} fill></Image>
+            <Image src={"/logo_w.png"} alt={"WigglePaw"} fill></Image>
           </div>
           <h1 className="my-auto mr-2 text-2xl font-bold text-white sm:text-3xl">
             WigglePaw
           </h1>
         </Link>
-        <ul className="my-auto ml-auto hidden sm:block">
-          <li className="header-li">
-            <div className="header-li-div">
+
+        {/* ----------------------Desktop---------------------- */}
+        <div className="hidden sm:flex">
+          <div className="my-auto h-fit">
+            <Link
+              href="/about"
+              className={`header-desktop-button
+                ${asPath.includes("/about") ? "header-at-page" : ""}`}
+            >
+              About
+            </Link>
+          </div>
+          {isLoggedIn && (
+            <div className="my-auto h-fit">
               <Link
-                href="/about"
-                className={
-                  "header-a" +
-                  `${asPath.includes("/about") ? " header-at-page" : ""}`
-                }
+                href="/Help"
+                className={`header-desktop-button
+                ${asPath.includes("/help") ? "header-at-page" : ""}`}
               >
-                About
+                Help
               </Link>
             </div>
-          </li>
+          )}
           {!isLoggedIn && (
-            <li className="header-li">
-              <div className="header-li-div">
+            <>
+              <div className="my-auto h-fit">
                 <Link
                   href={{
                     pathname: "/login",
                     query: { previousPage: previousPage },
                   }}
-                  className={
-                    "header-a" +
-                    `${
-                      asPath.includes("/login")
-                        ? " header-at-page pointer-events-none"
-                        : ""
-                    }`
-                  }
+                  className={`header-desktop-button
+                  ${
+                    asPath.includes("/login")
+                      ? "header-at-page pointer-events-none cursor-default"
+                      : ""
+                  }`}
                 >
                   Login
                 </Link>
               </div>
-            </li>
+
+              <Menu>
+                <div className="my-auto">
+                  <Menu.Button className={"header-desktop-button my-auto"}>
+                    Register
+                  </Menu.Button>
+                </div>
+                <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white p-[0.2rem] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-[0.1rem]">
+                    <Menu.Item>
+                      <Link
+                        href="/registerPetOwner"
+                        className={`
+                      header-dropdown group
+                      ${
+                        asPath.includes("/registerPetOwner")
+                          ? "header-at-page border-green-700 text-green-700"
+                          : "bg-green-700 hover:bg-green-500"
+                      }`}
+                      >
+                        Register Pet Owner
+                      </Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Link
+                        href="/registerPetSitter"
+                        className={`
+                      header-dropdown group
+                      ${
+                        asPath.includes("/registerPetSitter")
+                          ? "header-at-page border-green-700 text-green-700"
+                          : "bg-green-700 hover:bg-green-500"
+                      }`}
+                      >
+                        Register Pet Sitter
+                      </Link>
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Menu>
+            </>
           )}
+
           {isLoggedIn && (
-            <li className="header-li">
-              <div className="header-li-div">
-                <Link
-                  href="/help"
-                  className={
-                    "header-a" +
-                    `${asPath.includes("/help") ? " header-at-page" : ""}`
-                  }
-                >
-                  Help
-                </Link>
-              </div>
-            </li>
-          )}
-        </ul>
-      </nav>
-      {!isLoggedIn && (
-        <div className="relative my-auto hidden sm:flex">
-          <button
-            onClick={toggleRegister}
-            className={
-              "header-a my-auto h-fit" +
-              `${
-                asPath.includes("/registerPetOwner" || "/registerPetSitter")
-                  ? " header-at-page"
-                  : ""
-              }`
-            }
-          >
-            Register
-          </button>
-          <div
-            className="absolute top-full right-0 z-30 rounded bg-slate-300 px-2 py-1"
-            hidden={!openRegister}
-          >
-            <Link href="/registerPetOwner" className="header-register-link">
-              Register Pet Owner
-            </Link>
-            <Link href="/registerPetSitter" className="header-register-link">
-              Register Pet Sitter
-            </Link>
-          </div>
-        </div>
-      )}
-      {!isLoggedIn && status != "loading" && (
-        <div className="relative my-auto sm:hidden">
-          <button onClick={toggleRegister} className="header-a my-auto h-fit">
-            Menu
-          </button>
-          <div
-            className="absolute top-full right-0 z-30 rounded bg-slate-300 px-2 py-1"
-            hidden={!openRegister}
-          >
-            <Link
-              href="/about"
-              className={
-                "header-register-link bg-sky-700 hover:bg-sky-600" +
-                `${asPath.includes("/about") ? " header-at-page" : ""}`
-              }
-            >
-              About
-            </Link>
-            <Link
-              href={{
-                pathname: "/login",
-                query: { previousPage: previousPage },
-              }}
-              className={
-                "header-register-link" +
-                `${
-                  asPath.includes("/login")
-                    ? " header-at-page pointer-events-none cursor-default"
-                    : " bg-sky-700 hover:bg-sky-600"
-                }`
-              }
-            >
-              Login
-            </Link>
-            <Link
-              href="/registerPetOwner"
-              className={
-                "header-register-link" +
-                `${
-                  asPath.includes("/registerPetOwner")
-                    ? " header-at-page text-green-700"
-                    : ""
-                }`
-              }
-            >
-              Register Pet Owner
-            </Link>
-            <Link
-              href="/registerPetSitter"
-              className={
-                "header-register-link" +
-                `${
-                  asPath.includes("/registerPetSitter")
-                    ? " header-at-page text-green-700"
-                    : ""
-                }`
-              }
-            >
-              Register Pet Sitter
-            </Link>
-          </div>
-        </div>
-      )}
-      {isLoggedIn && (
-        <div className="relative my-auto ml-1">
-          <div className="relative m-2 flex h-[4rem] w-[4rem]">
-            <button onClick={toggleProfile} className="my-auto h-fit">
-              <Image
-                src={profileImageUri}
-                alt={"Icon"}
-                fill
-                className="rounded-xl"
-              ></Image>
-            </button>
-            <div
-              className="absolute top-full right-0 z-30 rounded bg-slate-300 px-2 py-1"
-              hidden={!openProfile}
-            >
-              <Link
-                href={`/user/${username!}`}
-                className={
-                  "header-menu-link" +
-                  `${
-                    asPath.includes("/user/" + username!)
-                      ? " header-at-page"
-                      : ""
-                  }`
-                }
-              >
-                Profile
-              </Link>
-              <Link
-                href="/about"
-                className={
-                  "header-menu-link sm:hidden" +
-                  `${asPath.includes("/about") ? " header-at-page" : ""}`
-                }
-              >
-                About
-              </Link>
-              <Link
-                href="/help"
-                className={
-                  "header-menu-link sm:hidden" +
-                  `${asPath.includes("/help") ? " header-at-page" : ""}`
-                }
-              >
-                Help
-              </Link>
-              <button onClick={logout}>
-                <Link href="/" className="header-menu-link">
-                  Logout
-                </Link>
-              </button>
+            <div className="relative my-auto">
+              <Menu>
+                <div className="my-auto">
+                  <Menu.Button
+                    className={"relative m-2 flex h-[4rem] w-[4rem]"}
+                  >
+                    <Image
+                      src={profileImageUri}
+                      alt={"Icon"}
+                      fill
+                      className="rounded-full"
+                    ></Image>
+                  </Menu.Button>
+                </div>
+                <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white p-[0.2rem] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-[0.1rem]">
+                    <Menu.Item>
+                      <Link
+                        href={`/user/${username}`}
+                        className={`
+                        header-dropdown group
+                        ${
+                          asPath.includes("/user/" + username)
+                            ? "header-at-page"
+                            : ""
+                        }`}
+                      >
+                        Profile
+                      </Link>
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-[0.1rem]">
+                    <Menu.Item>
+                      <button
+                        onClick={logout}
+                        className={`header-dropdown group`}
+                      >
+                        Logout
+                      </button>
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Menu>
             </div>
-          </div>
+          )}
         </div>
-      )}
+
+        {/* ----------------------Mobile---------------------- */}
+        {status != "loading" && (
+          <div className="relative my-auto sm:hidden">
+            <Menu>
+              {isLoggedIn ? (
+                <Menu.Button className={"relative m-2 flex h-[4rem] w-[4rem]"}>
+                  <Image
+                    src={profileImageUri}
+                    alt={"Icon"}
+                    fill
+                    className="rounded-full"
+                  ></Image>
+                </Menu.Button>
+              ) : (
+                <Menu.Button className={"header-mobile-menu my-auto h-fit"}>
+                  Menu
+                </Menu.Button>
+              )}
+
+              <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white p-[0.2rem] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {!isLoggedIn && (
+                  <>
+                    <div className="px-1 py-[0.1rem]">
+                      <Menu.Item>
+                        <Link
+                          href={{
+                            pathname: "/login",
+                            query: { previousPage: previousPage },
+                          }}
+                          className={`
+                            header-dropdown group
+                            ${
+                              asPath.includes("/login")
+                                ? "header-at-page pointer-events-none cursor-default"
+                                : ""
+                            }`}
+                        >
+                          Login
+                        </Link>
+                      </Menu.Item>
+                    </div>
+                    <div className="px-1 py-[0.1rem]">
+                      <Menu.Item>
+                        <Link
+                          href="/registerPetOwner"
+                          className={`
+                      header-dropdown group
+                      ${
+                        asPath.includes("/registerPetOwner")
+                          ? "header-at-page border-green-700 text-green-700"
+                          : "bg-green-700 hover:bg-green-500"
+                      }`}
+                        >
+                          Register Pet Owner
+                        </Link>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <Link
+                          href="/registerPetSitter"
+                          className={`
+                      header-dropdown group
+                      ${
+                        asPath.includes("/registerPetSitter")
+                          ? "header-at-page border-green-700 text-green-700"
+                          : "bg-green-700 hover:bg-green-500"
+                      }`}
+                        >
+                          Register Pet Sitter
+                        </Link>
+                      </Menu.Item>
+                    </div>
+                  </>
+                )}
+                {isLoggedIn && (
+                  <>
+                    <div className="px-1 py-[0.1rem]">
+                      <Menu.Item>
+                        <Link
+                          href={`/user/${username}`}
+                          className={`
+                        header-dropdown group
+                        ${
+                          asPath.includes("/user/" + username)
+                            ? "header-at-page"
+                            : ""
+                        }`}
+                        >
+                          Profile
+                        </Link>
+                      </Menu.Item>
+                    </div>
+                  </>
+                )}
+                <div className="px-1 py-[0.1rem]">
+                  <Menu.Item>
+                    <Link
+                      href="/about"
+                      className={`
+                      header-dropdown group
+                      ${asPath.includes("/about") ? "header-at-page" : ""}`}
+                    >
+                      About
+                    </Link>
+                  </Menu.Item>
+                </div>
+                <div className="px-1 py-[0.1rem]">
+                  <Menu.Item>
+                    <Link
+                      href="/help"
+                      className={`
+                      header-dropdown group
+                      ${asPath.includes("/help") ? " header-at-page" : ""}`}
+                    >
+                      Help
+                    </Link>
+                  </Menu.Item>
+                </div>
+                <div className="px-1 py-[0.1rem]">
+                  <Menu.Item>
+                    <button
+                      onClick={logout}
+                      className={`header-dropdown group`}
+                    >
+                      Logout
+                    </button>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Menu>
+          </div>
+        )}
+      </nav>
     </span>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,9 @@ module.exports = {
     extend: {     
       colors: {
       'bg-box-main': '#CCB0A4',
-      'bg-main': '#E5D4C4'
-      
+      'bg-main': '#E5D4C4',
+      "wp-blue": "#213951",
+      "wp-light-blue": "#3b6691",
     },},
   },
   plugins: [],
