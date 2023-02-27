2a1b6214ea2b6563f09ae031c4ad58bba750af4f
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-02-27 21:53:33 +0700

    Update reviewPage.tsx

--- src/pages/reviewPage.tsx
+++ src/pages/reviewPage.tsx
@@ -19,12 +19,14 @@ const reviewPage: NextPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const OnSubmit = async (data: any) => {
+  const OnSubmit = (data: any) => {
     console.log(data.review);
+    setHasReview(true);
     closeModal();
   };
   const [isOpen, setIsOpen] = useState(false);
   const [rating, setRating] = useState(0);
+  const [hasReview, setHasReview] = useState(false);
   function closeModal() {
     setIsOpen(false);
   }
@@ -40,25 +42,27 @@ const reviewPage: NextPage = () => {
     // Set the initial value
     setRating(0);
   };
-  const avgrate = 4.2;
   return (
     <div>
       <Header />
       <div className="mt-4 flex h-full flex-col items-center text-xl">
         <h1>Reviews</h1>
         <h2>Average rating : {rating}</h2>
-        <div className="h-100 w-100 box-content flex flex-col items-center rounded-md border-4 bg-amber-50 p-4">
-          <button onClick={openModal}>Write Review</button>
-          <Rating
-            initialValue={rating}
-            SVGclassName="inline-block"
-            size={30}
-            readonly
-          ></Rating>
-          <h1 className="text-base">
-            I think this pet sitter is good she is friendly and cost is cheap
-          </h1>
-        </div>
+        {!hasReview && <button onClick={openModal}>Write Review</button>}
+        {hasReview && <button onClick={openModal}>Edit Review</button>}
+        {hasReview && (
+          <div className="h-100 w-100 box-content flex flex-col items-center rounded-md border-4 bg-amber-50 p-4">
+            <Rating
+              initialValue={rating}
+              SVGclassName="inline-block"
+              size={30}
+              readonly
+            ></Rating>
+            <h1 className="text-base">
+              I think this pet sitter is good she is friendly and cost is cheap
+            </h1>
+          </div>
+        )}
 
         <h2>Review from other users</h2>
       </div>
@@ -75,7 +79,6 @@ const reviewPage: NextPage = () => {
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
-
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
@@ -90,11 +93,11 @@ const reviewPage: NextPage = () => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="flex flex-col items-center text-lg font-medium leading-6 text-gray-900"
                   >
                     Write your review
                   </Dialog.Title>
-                  <div>
+                  <div className="flex flex-col items-center">
                     <form onSubmit={OnSubmit}>
                       <h1>Enter star</h1>
                       <Rating
@@ -106,11 +109,16 @@ const reviewPage: NextPage = () => {
                       />
                       <br />
                       <h1>Enter your review</h1>
-                      <input id="review" type="text"></input>
+                      <input
+                        id="review"
+                        type="text"
+                        className="block w-full border border-black bg-gray-50"
+                      ></input>
                       <br />
-                      <button onClick={closeModal}>Cancel</button>
-                      <br />
-                      <button type="submit">Submit</button>
+                      <div className="grid grid-cols-2">
+                        <button onClick={closeModal}>Cancel</button>
+                        <button type="submit">Submit</button>
+                      </div>
                     </form>
                   </div>
                 </Dialog.Panel>
