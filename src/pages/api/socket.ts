b7fbc99162f50a05499012ac8204ffb74d21c501
Theresa <thutgreybear@gmail.com>
2023-03-20 21:08:29 +0700

    feat:done join room part

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -13,9 +13,22 @@ export default function SocketHandler(req, res) {
   res.socket.server.io = io;
 
   const onConnection = (socket) => {
-    console.log("conected");
+    console.log("conected", socket.id);
+
+    socket.on("joinroom1", (msg) => {
+      //join mechanic working as expected!!
+      console.log("join room 1");
+      socket.join("1");
+    });
+
+    socket.on("joinroom2", (msg) => {
+      console.log("join room 2");
+      socket.join("2");
+    });
+
     socket.on("createdMessage", (msg) => {
-      socket.emit("newIncomingMessage", "FUCK!");
+      io.to("1").emit("newIncomingMessage", msg + "sss");
+      console.log(socket.id);
       console.log(msg);
     });
   };
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -12,27 +12,62 @@ const Chat = () => {
     socketInitializer();
   }, []);
 
+  const [msg, setMSG] = useState<string>();
+
   const socketInitializer = async () => {
     // We just call it because we don't need anything else out of it
     await fetch("/api/socket");
 
     socket = io();
 
-    socket.on("newIncomingMessage", (msg) => {
-      console.log(msg);
+    socket.on("newIncomingMessage", (msg1) => {
+      console.log(msg1);
+      console.log(socket.id);
+      setMSG(msg1);
     });
   };
 
   const sendMessage = async () => {
     console.log("to");
-    socket.emit("createdMessage", "hi2");
+  };
+
+  const sendForm = (e) => {
+    e.preventDefault();
+    console.log(e.target.chat.value);
+    socket.emit("createdMessage", e.target.chat.value);
   };
 
   return (
     <>
       <Header></Header>
-      Chat la
+      {msg}
       <div onClick={sendMessage} className="h-8 w-8 bg-black "></div>
+      <div className="pl-20">
+        <span>
+          <form onSubmit={sendForm}>
+            <input id="chat" className="border-2 border-black"></input>
+            <button className="ml-4 border-2 border-blue-700 bg-blue-300 px-2">
+              Send
+            </button>
+          </form>
+        </span>
+      </div>
+      <button
+        onClick={() => {
+          socket.emit("joinroom1", "room1");
+        }}
+        className="h-5 w-20 bg-red-300"
+      >
+        Room1
+      </button>
+      <button
+        onClick={() => {
+          socket.emit("joinroom2", "room2");
+        }}
+        className="h-5 w-20 bg-blue-300"
+      >
+        Room2
+      </button>
     </>
   );
 };
