b4c9f29ee8432702c7ad91504f37961565b2230a
Tuchtham Sungkameka <touch0869@gmail.com>
2023-04-25 00:42:19 +0700

    refactor(cypress): register pet owner

--- cypress/e2e/registerOwner.cy.ts
+++ cypress/e2e/registerOwner.cy.ts
@@ -1,68 +1,80 @@
+import { firstNames, petTypes } from "./../../src/seed/pool";
+import { z } from "zod";
+import { Ownerfields } from "../pool/registerOwner";
+import { makeOwner } from "../../src/seed/db";
 // This is a sample test file to show how to write tests with Cypress
 //
 // Note that while this file demonstrates how to write tests,
 // it does not imply a good structure, naming convention, organization, etc.
 // (ie. It work, but I have no idea if this is a good tests)
 
-// import { makeOwner } from '../../src/seed/db';
+/* const existingOwner = {
+  code: "Owner123",
+  firstName: "Adam",
+  lastName: "Smith",
+  phone: "0987654321",
+  address: "Adam Street Smith District",
+  imageUri: "",
+  petTypes: ["Dog", "Cat"],
+};
 
-// const existingOwner = {
-//   code: 'Owner123',
-//   firstName: 'Adam',
-//   lastName: 'Smith',
-//   phone: '0987654321',
-//   address: 'Adam Street Smith District',
-//   imageUri: '',
-//   petTypes: ['Dog', 'Cat'],
-// };
+async function makeExistingOwner() {
+  await makeOwner(
+    "Owner123",
+    "Adam",
+    "Smith",
+    "0987654321",
+    "Adam Street Smith District",
+    "",
+    ["Dog", "Cat"]
+  );
+} */
 
-// async function makeExistingOwner() {
-//   await makeOwner(
-//     'Owner123',
-//     'Adam',
-//     'Smith',
-//     '0987654321',
-//     'Adam Street Smith District',
-//     '',
-//     ['Dog', 'Cat']
-//   );
-//   return;headlessui-me'
-// }
+function validInput(fields: {
+  username: string;
+  password: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  address: string;
+  type: string;
+}) {
+  cy.get("#firstName").type(fields.firstname);
+  cy.get("#lastName").type(fields.lastname);
+  cy.get("#email").type(fields.email);
+  cy.get("#address").type(fields.address);
+  cy.get("#phone").type(fields.phone);
+  cy.get("#username").type(fields.username);
+  cy.get("#password").type(fields.password);
+  cy.get("#confirmPassword").type(fields.password);
+  cy.get("#type").type(fields.type);
+  cy.contains("Next").click();
+  cy.contains("2/2").should("exist");
+  cy.get("#holderName").type(fields.firstname + " " + fields.lastname);
+  cy.get("#cardNo").type("4242424242424242");
+  cy.get("#expDate").type("2025-01-01");
+  cy.get("#cvv").type("666");
+  cy.get(".col-span-2.items-center > .mr-2").check();
+  cy.get("#register-button").click();
+}
 
-// await makeExistingOwner();
-
-// describe("Can traverse to register owner page", () => {
-//   it("Click buttons to register owner page", () => {
-//     cy.visit("/");
-//     cy.contains("Register").click();
-//     cy.contains("Register Pet Owner").click();
-//     cy.url().should("include", "/registerPetOwner");
-//   });
-// });
+describe("Can traverse to register owner page", () => {
+  it("Click buttons to register owner page", () => {
+    cy.visit("/");
+    cy.contains("Register").click();
+    cy.contains("Register Pet Owner").click();
+    cy.url().should("include", "/registerPetOwner");
+  });
+});
 
 describe("Fields validation", () => {
   beforeEach(() => {
     cy.visit("/registerPetOwner");
   });
 
-  it("TC1-1", () => {
-    cy.get("#firstName").type("Bobby");
-    cy.get("#lastName").type("Testing");
-    cy.get("#email").type("bobby123@hotmail.com");
-    cy.get("#address").type("Bangkok");
-    cy.get("#phone").type("0818118111");
-    cy.get("#username").type("bobby123");
-    cy.get("#password").type("Bobby123_");
-    cy.get("#confirmPassword").type("Bobby123_");
-    cy.get("#type").type("Dog Cat ");
-    cy.contains("Next").click();
-    cy.contains("2/2").should("exist");
-    cy.get("#holderName").type("Bobby Testing");
-    cy.get("#cardNo").type("4242424242424242");
-    cy.get("#expDate").type("2025-01-01");
-    cy.get("#cvv").type("666");
-    cy.get(".col-span-2.items-center > .mr-2").check();
-    cy.get("#register-button").click();
+  it("TC1-1", async () => {
+    validInput(Ownerfields["TC1-1"]);
   });
 });
 
